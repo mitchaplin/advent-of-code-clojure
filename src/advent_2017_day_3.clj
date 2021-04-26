bf12f9abd9b226dd078c030e7ca286182f2c8658
Mitch Aplin <mitch.aplin@Mitchs-MacBook-Pro.local>
2021-04-25 21:35:31 -0500

    2017 day 6

--- src/advent_2017_day_3.clj
+++ src/advent_2017_day_3.clj
@@ -2,5 +2,10 @@
   (:require [clojure.string :as str]))
 
 (def input 368078)
-
+(def test-input (range 1 26))
 (def num-range (range input))
+
+(defn build-board
+  []
+  (loop
+    [x (count test-input)]))
