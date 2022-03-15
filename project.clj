7d54c03e20cb0692685064f1e949b38600515f0c
Mitch Aplin <mitchaplin@gmail.com>
2022-03-15 18:54:04 -0500

    Test commit

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject advent "0.1.0-SNAPSHOT"
   :description "advent"
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/math.combinatorics "0.1.6"]
                  [org.clojure/math.numeric-tower "0.0.4"]])
