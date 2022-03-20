5d1ca7bbdeb75b0ed1ed8e749efa3c38e3c34818
Mitch Aplin <mitchaplin@gmail.com>
2022-03-20 13:22:44 -0500

    2021 Day 3 p1

--- src/advent_2021_day_3.clj
+++ src/advent_2021_day_3.clj
@@ -1,8 +1,44 @@
 (ns advent_2021_day_3
   (:require [clojure.string :as str]))
 
-(def raw (slurp "resources/2021/day_2.txt"))
+(def raw (slurp "resources/2021/day_3_practice.txt"))
 (def processed (map #(str/split % #" ") (str/split-lines raw)))
 
+;PART 1 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+(defn pluck-value
+  [v f]
+  (if (f (val (first v)) (val (second v)))
+    (ffirst v)
+    (first (second v))))
 
+(defn p1
+  []
+  (loop [p processed
+         tot 0
+         acc []]
+    (if (<= (count (ffirst processed)) tot)
+      (* (Long/parseLong (apply str (map #(pluck-value % <) acc)) 2)
+         (Long/parseLong (apply str (map #(pluck-value % >) acc)) 2))
+      (recur
+        p
+        (inc tot)
+        (conj
+          acc
+          (frequencies
+            (map #(nth (first %) tot) p)))))))
 
+;PART 2 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+(defn p2
+  [g]
+  (loop [p processed
+         tot 0
+         acc []]
+    (if (<= (count (ffirst processed)) tot)
+      acc
+      (recur
+        p
+        (inc tot)
+        (conj
+          acc
+          (frequencies
+            (map #(nth (first %) tot) p)))))))
