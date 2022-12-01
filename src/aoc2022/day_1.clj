4e2e9e42b7aa7b1cd6f6f3baf1823d2e357ac183
Mitch Aplin <mitchaplin@gmail.com>
2022-11-30 23:16:04 -0600

    2022 Day-1

--- src/aoc2022/day_1.clj
+++ src/aoc2022/day_1.clj
@@ -1,1 +1,16 @@
-(ns aoc2022.day-1)
+(ns aoc2022.day-1
+  (:require [clojure.string :as str]))
+
+(def raw
+  (->> "resources/2022/day_1.txt"
+       (slurp)))
+
+(def processed (map #(str/split % #"\n") (str/split raw #"\n\n")))
+
+(defn part-1
+  []
+  (apply max (map (fn [x] (reduce + (map #(Integer/parseInt %) x))) processed)))
+
+(defn part-2
+  []
+  (reduce + (take 3 (reverse (sort (map (fn [x] (reduce + (map #(Integer/parseInt %) x))) processed))))))
