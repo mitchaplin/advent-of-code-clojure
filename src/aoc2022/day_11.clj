a5350d3ca5671795f8fd7a164056bf95096a0461
Mitch Aplin <mitchaplin@gmail.com>
2022-12-12 11:14:05 -0600

    2022 Day 11 Complete

--- src/aoc2022/day_11.clj
+++ src/aoc2022/day_11.clj
@@ -1,7 +1,16 @@
 (ns aoc2022.day-11
-  (:require [clojure.string :as str]))
+  (:require
+    [clojure.math.numeric-tower :as math]
+    [clojure.string :as str]))
 
-(def raw (last (first (group-by count (partition-by empty? (map str/trim (utils/read-file "resources/2022/day_11.txt")))))))
+(def raw (->> (utils/read-file "resources/2022/day_11.txt")
+              (map str/trim)
+              (partition-by empty?)
+              (group-by count)
+              (vals)
+              (first)))
+
+(def monkey-list-practice [:0 :1 :2 :3])
 (def monkey-list [:0 :1 :2 :3 :4 :5 :6 :7])
 
 (defn parse-op
@@ -23,45 +32,60 @@
 
 (defn parse-input
   [i [_ items val divi to-true to-false]]
-  (let [x (mapv utils/parse-int (str/split (str/replace (second (str/split items #": "))  #"," "") #" "))
+  (let [x (mapv utils/parse-int (str/split (str/replace (second (str/split items #": ")) #"," "") #" "))
         to-true (keyword (last (str/split to-true #" ")))
         to-false (keyword (last (str/split to-false #" ")))]
     {(keyword (str i))
-     {:items x
-      :parse-fn (parse-op (str/split (second (str/split val #"= ")) #" "))
+     {:items        x
+      :parse-fn     (parse-op (str/split (second (str/split val #"= ")) #" "))
       :divisible-by (utils/parse-int (last (str/split divi #" ")))
-      :to-true to-true
-      :to-false to-false
-      :total-count (count x)}}))
+      :to-true      to-true
+      :to-false     to-false
+      :total-count  0}}))
+
+(def initial-monkey-map (into {} (map-indexed (fn [i v] (parse-input i v)) raw)))
+
+(def divi (reduce math/lcm (map :divisible-by (vals initial-monkey-map))))
 
 (defn update-monkey-map
-  [i cm mm]
-  (println i)
-  (let [current-map cm
-        new-val ((:parse-fn current-map) i)
-        is-divisible? (= 0 (mod new-val (:divisible-by current-map)))
-        updated-val (int (Math/floor (/ new-val 3)))]
-    (if is-divisible? (assoc-in mm [(:to-true current-map) :items] (vec (cons updated-val (:items current-map))))
-                      (assoc-in mm [(:to-false current-map) :items] (vec (cons updated-val (:items current-map)))))))
+  [i cm mm is-p1]
+  (let [new-val ((:parse-fn cm) i)
+        updated-val (if is-p1 (Math/floorDiv new-val 3) (mod new-val divi))
+        is-divisible? (= 0 (mod updated-val (:divisible-by cm)))]
+    (if is-divisible? (assoc-in mm [(:to-true cm) :items] (conj (:items (get mm (:to-true cm))) updated-val))
+                      (assoc-in mm [(:to-false cm) :items] (conj (:items (get mm (:to-false cm))) updated-val)))))
 
 (defn monkey-bizz
-  [m mm]
-  (println m (:to-true (m mm)) mm)
+  [m mm is-p1]
   (let [cm (m mm)
         updated-map (assoc-in mm [m :total-count] (+ (count (:items cm))
                                                      (:total-count cm)))]
     (loop [i (:items cm)
            monkey-map updated-map]
       (if (empty? i)
-        (assoc monkey-map [m :items] [])
-        (recur  (rest i)
-                (update-monkey-map (first i) cm monkey-map))))))
-
-(def initial-monkey-map (into {} (map-indexed  (fn [i v] (parse-input i v)) raw)))
+        (assoc-in monkey-map [m :items] [])
+        (recur (rest i)
+               (update-monkey-map (first i) cm monkey-map is-p1))))))
 
 (defn part-1
-  []
+  [m ml is-p1]
   (reduce (fn [new-map k]
-              (monkey-bizz k new-map))
-          initial-monkey-map
-          monkey-list))
+            (monkey-bizz k new-map is-p1))
+          m
+          ml))
+
+(defn run-times
+  [n ml is-p1]
+  (loop [iter n
+         new-map initial-monkey-map]
+    (if (zero? iter)
+      new-map
+      (recur (dec iter)
+             (part-1 new-map ml is-p1)))))
+
+(defn count-total
+  [is-p1 num]
+  (reduce * (take-last 2 (sort (map :total-count (vals (run-times num monkey-list is-p1)))))))
+
+;(count-total false 10000)
+;(count-total true 20)
