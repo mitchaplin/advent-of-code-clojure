def2f0860951c9bb9f6b4d2842a30b541cdf4202
Mitch Aplin <mitchaplin@gmail.com>
2022-12-12 22:37:43 -0600

    2022 Day 12 P1

--- src/aoc2022/day_12.clj
+++ src/aoc2022/day_12.clj
@@ -2,17 +2,17 @@
   (:require [clojure.math.combinatorics :as combo]
             [clojure.string :as str]))
 
-(def raw (utils/read-file "resources/2022/day_12.txt"))
+(def raw (utils/read-file "resources/2022/day_12_practice.txt"))
 
-(def start [0 20])
-(def end [88 20])
-(def start-kw :020)
-(def end-kw :8820)
+;(def start [0 20])
+;(def end [88 20])
+;(def start-kw :020)
+;(def end-kw :8820)
 
-;(def start [0 0])
-;(def end [5 2])
-;(def start-kw :00)
-;(def end-kw :52)
+(def start [0 0])
+(def end [5 2])
+(def start-kw :00)
+(def end-kw :52)
 
 (def max-length (dec (count (first raw))))
 (def max-height (dec (count raw)))
@@ -43,21 +43,15 @@
 
 (def generated-neighbors
   (reduce (fn [km [a b]] (assoc-in km [(keyword (str a b)) :neighbors]
-                                 (into (sorted-map) (remove nil? (mapv (fn [[x y]] (if (>= (inc (:val ((keyword (str a b)) processed)))
-                                                                                           (:val ((keyword (str x y)) processed)))
+                                 (into (sorted-map) (remove nil? (mapv (fn [[x y]] (if (<= (:val ((keyword (str x y)) processed))
+                                                                                           (inc (:val ((keyword (str a b)) processed))))
 
                                                                                      {(keyword (str x y))
-                                                                                      (:val ((keyword (str a b)) processed))} nil))
+                                                                                      1}))
                                                                        (generate-neighbor-points [a b]))))))
           processed
           all-points))
 
 (def graph (into (sorted-map) (map (fn [x] {(key x) (:neighbors (val x))}) generated-neighbors)))
 
-(defn set-keys-to-1
-  [collection]
-  (reduce-kv (fn [acc k v] (assoc acc k 1)) {} collection))
-
-(def initial-map (into (sorted-map) (reduce-kv (fn [acc k v] (assoc acc k (set-keys-to-1 v))) {} graph)))
-
-;(utils/dijkstra testing start-kw end-kw)
+(utils/dijkstra graph start-kw end-kw)
