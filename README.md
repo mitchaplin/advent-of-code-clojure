afad46fbd693548566063141c2b1fb75cf5ea3be
Mitch Aplin <mitchaplin@gmail.com>
2025-03-03 21:45:53 -0600

    Begin day_1 2024

--- README.md
+++ README.md
@@ -3,5 +3,4 @@
 > [Advent of Code](https://adventofcode.com/) is an Advent calendar of small
 > programming puzzles for a variety of skill sets and skill levels that can be solved
 > in any programming language you like.
-
-> This repo contains my solutions in Clojure. 
+> This repo contains my solutions in Clojure.
--- src/aoc2024/day_1.clj
+++ src/aoc2024/day_1.clj
@@ -1,11 +1,13 @@
 (ns aoc2024.day-1
   (:require
-   [clojure.string :as str]))
-(def raw (slurp "resources/202/day_1.txt"))
+   [clojure.string :as str]
+   [clojure.math.numeric-tower :as math]))
+(def raw (slurp "resources/2024/day_1.txt"))
 (def processed (str/split (str/replace raw #" " "") #"\n"))
-(def transformed-ints (partition 2 (map (fn [item] (map #(Integer/parseInt (str %)) item)) processed)))
-
-(->> transformed-ints
-     (map (fn [x] (map #(reduce + %) x)))
-     (map #(reduce - %))
-     (reduce +))
+;; (def transformed-ints (partition 2 (map (fn [item] (sort (map #(Integer/parseInt (str %)) item))) processed)))
+(def transformed-ints (partition 2 (map (fn [item] (sort (map #(Integer/parseInt (str %)) item))) processed)))
+transformed-ints
+;; (->> transformed-ints
+   ;; (map #(zipmap (first %) (second %))))
+   ;; (map #(math/abs (reduce - %)))
+   ;; (reduce +))
